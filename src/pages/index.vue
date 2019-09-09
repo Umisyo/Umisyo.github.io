0f8fb68912350e446ea52972dd1869b59623e0c7
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-09 22:49:50 +0900

    feat: webp対応

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
   div.top
-    img.top__icon(src="~/assets/images/Umisyo.jpg" loading="lazy" decoding="async" alt="Souta Kusunoki")
+    img.top__icon(src="~/assets/images/icon.webp" loading="lazy" decoding="async" alt="Souta Kusunoki")
     h1.top__name Souta Kusunoki
 </template>
