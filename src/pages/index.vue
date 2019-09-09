479510545dc8fff18362dacf70e78581e6277975
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-10 00:03:12 +0900

    add: social icons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,14 @@
 <template lang="pug">
   div.top
-    img.top__icon(src="~/assets/images/icon.webp" loading="lazy" decoding="async" alt="Souta Kusunoki")
+    img.top__icon(src="~/assets/images/icon.webp" alt="Souta Kusunoki")
     h1.top__name Souta Kusunoki
+    div.top__social
+      a.top__social-link(href="http://github.com/Umisyo")
+        img(src="~/assets/images/github-icon.webp")
+      a.top__social-link-instagram(href="http://instagram.com/ihcamonoihS")
+        img(src="~/assets/images/instagram-icon.webp")
+      a.top__social-link(href="http://twitter.com/ihcamonoihS")
+        img(src="~/assets/images/twitter-icon.webp")
+      a.top__social-link(href="http://facebook.com/ihcamonoihS")
+        img(src="~/assets/images/facebook-icon.webp")
 </template>
