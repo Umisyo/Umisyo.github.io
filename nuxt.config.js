ad12e076dfdfcd21ffc9d7917f4d952a330f8cb0
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-09 22:49:19 +0900

    refactor: amp化をpluginに

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,4 @@
-const modifyHtml = html => {
-  // Add amp-custom tag to added CSS
-  html = html.replace(/<style data-vue-ssr/g, '<style amp-custom data-vue-ssr')
-  // Remove every script tag from generated HTML
-  html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '')
-  // Add AMP script before </head>
-  const ampScript =
-    '<script async src="https://cdn.ampproject.org/v0.js"></script>'
-  const ampSocial =
-    '<script async custom-element="amp-social-share" src="https://cdn.ampproject.org/v0/amp-social-share-0.1.js"></script>'
-  html = html.replace('</head>', ampScript + ampSocial + '</head>')
-  return html
-}
+const ampify = require('./src/plugins/ampify')
 
 export default {
   mode: 'universal',
@@ -61,13 +49,13 @@ export default {
     extend(config, ctx) {}
   },
   hooks: {
-    // This hook is called before generatic static html files for SPA mode
+    // This hook is called before saving the html to flat file
     'generate:page': page => {
-      page.html = modifyHtml(page.html)
+      page.html = ampify(page.html)
     },
-    // This hook is called before rendering the html to the browser
+    // This hook is called before serving the html to the browser
     'render:route': (url, page, { req, res }) => {
-      page.html = modifyHtml(page.html)
+      page.html = ampify(page.html)
     }
   }
 }
