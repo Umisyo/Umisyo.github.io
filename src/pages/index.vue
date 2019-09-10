89275a2b1c2c3502526edf2a31d05f7cafcb11f5
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-10 12:01:01 +0900

    fix: ampの仕様に合わせてスタイルの指定の仕方を変更

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,11 +4,11 @@
     h1.top__name Souta Kusunoki
     div.top__social
       a.top__social-link(href="http://github.com/Umisyo")
-        img(src="~/assets/images/github-icon.webp" width="10rem" height="10rem")
+        img(src="~/assets/images/github-icon.webp" width="4em" height="4em")
       a.top__social-link-instagram(href="http://instagram.com/ihcamonoihS")
-        img(src="~/assets/images/instagram-icon.webp" width="10rem" height="10rem")
+        img(src="~/assets/images/instagram-icon.webp" width="4em" height="4em")
       a.top__social-link(href="http://twitter.com/ihcamonoihS")
-        img(src="~/assets/images/twitter-icon.webp" width="10rem" height="10rem")
+        img(src="~/assets/images/twitter-icon.webp" width="4em" height="4em")
       a.top__social-link(href="http://facebook.com/ihcamonoihS")
-        img(src="~/assets/images/facebook-icon.webp" width="10rem" height="10rem")
+        img(src="~/assets/images/facebook-icon.webp" width="4em" height="4em")
 </template>
