ffdc9d8fbab4b2f89d199155adbd6aed356191ca
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-10 22:10:01 +0900

    refactor:

--- src/pages/About/index.vue
+++ src/pages/About/index.vue
@@ -3,13 +3,13 @@
         img.about__face(src="~/assets/images/about/face.webp" alt="face" decoding="async" loading="lazy")
         h2.about__title Souta Kusunoki
         h3.about__aka a.k.a そーとく from Saitama, Japan・kusunokisouta@gmail.com
-        p.about-content 2000/10/10
+        p.about-content birthday: 2000.10.10
         p.about-content 高専を中退して株式会社ゆめみでフロントエンドエンジニアとした働いています
         p.about-content Pythonを用いたバックエンド開発、Nuxt.jsやReactによるフロントエンド開発、Arduino等を用いたIoTなどやっています
         ul.about__list
             li.about__list-content
                 img.about__list-content-img(src="~/assets/images/about/company.webp" alt="Company" width="1em" height="1em")
-                p.about__list-content-text 株式会社ゆめみ
+                p.about__list-content-text 株式会社ゆめみ since 2019.06.01
             li.about__list-content
                 img.about__list-content-img(src="~/assets/images/about/school.webp" alt="School" width="1em" height="1em")
                 p.about__list-content-text 東京工業高等専門学校 電子工学科 3年次中退
