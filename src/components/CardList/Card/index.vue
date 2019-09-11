e74b2ed52ff1a4b4428a5b588bd63f40cb128dbe
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-11 12:08:57 +0900

    feat: Card List コンポーネント

---
+++ src/components/CardList/Card/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    section.card
+        img.card__img(:src="imgSrc")
+        h3.card__title {{ title }}
+        p.card__text {{ text }}
+</template>
+
+<script>
+export default {
+    props: {
+        imgSrc: String,
+        title: String,
+        text: String
+    }
+}
+</script>
---
+++ src/components/CardList/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    div.card-list
+        Card(v-for="(content, index) in content_list" :key="index" :imgSrc="content.img" :title="content.title" :text="content.text")
+</template>
+
+<script>
+export default {
+    components: {
+        Card: () => import('~/components/CardList/Card')
+    },
+    props: {
+        content_list: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
