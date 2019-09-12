3f88ee8332fca2bdadd2c744e25b72307c40ee00
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-12 16:35:43 +0900

    refactor: imgタグに直接大きさを指定したほうが体感速度が上がるので修正

--- src/pages/amp/index.vue
+++ src/pages/amp/index.vue
@@ -6,7 +6,7 @@
       a.top__social-link(href="http://github.com/Umisyo")
         img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/github-icon_xq1ml1.webp" width="4em" height="4em" alt="Github")
       a.top__social-link-instagram(href="http://instagram.com/ihcamonoihS")
-        img(src="~/assets/images/socials/instagram-icon.webp" width="4em" height="4em" alt="Instagram")
+        img(src="https://res.cloudinary.com/umisyo/image/upload/v1568269864/Portfolio/instagram-icon_pj3wv5.webp" width="4em" height="4em" alt="Instagram")
       a.top__social-link(href="http://twitter.com/ihcamonoihS")
         img(src="https://res.cloudinary.com/umisyo/image/upload/v1568269864/Portfolio/instagram-icon_pj3wv5.webp" width="4em" height="4em" alt="Twitter")
       a.top__social-link(href="http://facebook.com/ihcamonoihS")
