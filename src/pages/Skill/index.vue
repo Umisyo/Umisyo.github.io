3b38d245c5983a139a4a68478c69879f33880c07
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-12 16:35:57 +0900

    feat: Skillページの雛形完成

--- src/pages/Skill/index.vue
+++ src/pages/Skill/index.vue
@@ -1,4 +1,40 @@
 <template lang="pug">
     div.skill
-        p [WIP]:工事中です
+        h2 Skills
+        CardList(:content_list="content_list")
+
 </template>
+
+<script>
+export default {
+  components: {
+    CardList: () => import("~/components/CardList")
+  },
+  data() {
+    return {
+      content_list: [
+        {
+          img: 'https://res.cloudinary.com/umisyo/image/upload/v1568269864/Portfolio/icon_zmntgo.webp',
+          title: "Python3",
+          text: "Django, Flask, and other..."
+        },
+        {
+          title: "JavaScript",
+          text: "Vue/Nuxt, React, Express"
+        },
+        {
+          title: "PHP",
+          text: "Laravel"
+        },
+        {
+          title: "HTML/CSS",
+          text: "sass, bem"
+        },
+        {
+          title: ""
+        }
+      ]
+    };
+  }
+};
+</script>
