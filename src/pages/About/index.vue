b7e1948a59a66418aeab2429e9248bae74ee6eef
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-12 16:06:34 +0900

    refactor: imageをCDN経由で読み込むように変更

--- src/pages/About/index.vue
+++ src/pages/About/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     div.about
-        img.about__face(src="~/assets/images/about/face.webp" alt="face" decoding="async" loading="lazy")
+        img.about__face(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269851/Portfolio/face_ckkwdi.webp" alt="face" decoding="async" loading="lazy")
         h2.about__title Souta Kusunoki
         h3.about__aka a.k.a そーとく from Saitama, Japan・kusunokisouta@gmail.com
         p.about-content birthday: 2000.10.10
@@ -8,13 +8,13 @@
         p.about-content Pythonを用いたバックエンド開発、Nuxt.jsやReactによるフロントエンド開発、Arduino等を用いたIoTなどやっています
         ul.about__list
             li.about__list-content
-                img.about__list-content-img(src="~/assets/images/about/company.webp" alt="Company" width="1em" height="1em")
+                img.about__list-content-img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269850/Portfolio/company_cichxt.webp" alt="Company" width="1em" height="1em")
                 p.about__list-content-text 株式会社ゆめみ since 2019.06.01
             li.about__list-content
-                img.about__list-content-img(src="~/assets/images/about/school.webp" alt="School" width="1em" height="1em")
+                img.about__list-content-img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269850/Portfolio/school_iqfrjt.webp" alt="School" width="1em" height="1em")
                 p.about__list-content-text 東京工業高等専門学校 電子工学科 3年次中退
             li.about__list-content
-                img.about__list-content-img(src="~/assets/images/about/favoritte.webp" alt="Favoritte" width="1em" height="1em")
+                img.about__list-content-img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269850/Portfolio/favoritte_mdiyf8.webp" alt="Favoritte" width="1em" height="1em")
                 p.about__list-content-text 旅行, 読書, 音楽鑑賞(ガレージロック, ヒップホップ, ポストロック, アニソン, etc...)
             
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
-  div.top
-    img.top__icon(src="~/assets/images/socials/icon.webp" alt="icon")
+  section.top
+    img.top__icon(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/icon_zmntgo.webp" alt="icon")
     h1.top__name Souta Kusunoki
     div.top__social
       a.top__social-link(href="http://github.com/Umisyo")
-        img(src="~/assets/images/socials/github-icon.webp"  alt="Github")
+        img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/github-icon_xq1ml1.webp"  alt="Github")
       a.top__social-link-instagram(href="http://instagram.com/ihcamonoihS")
-        img(src="~/assets/images/socials/instagram-icon.webp" alt="Instagram")
+        img(src="https://res.cloudinary.com/umisyo/image/upload/v1568269864/Portfolio/instagram-icon_pj3wv5.webp" alt="Instagram")
       a.top__social-link(href="http://twitter.com/ihcamonoihS")
-        img(src="~/assets/images/socials/twitter-icon.webp" alt="Twitter")
+        img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269865/Portfolio/twitter-icon_iofxq1.webp" alt="Twitter")
       a.top__social-link(href="http://facebook.com/ihcamonoihS")
-        img(src="~/assets/images/socials/facebook-icon.webp" alt="Facebook")
+        img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/facebook-icon_dwhvaj.webp" alt="Facebook")
 </template>
