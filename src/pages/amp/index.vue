ef6b55cd3a85458e8cad21923282bf74bebd2475
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-16 17:55:17 +0900

    change: 画像をCDN経由で読み込むように

--- src/pages/amp/index.vue
+++ src/pages/amp/index.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
   div.top
-    img.top__icon(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/icon_zmntgo.webp" width="10em" height="10em" alt="icon")
+    img.top__icon(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/icon_zmntgo.webp" width="100" height="100" alt="icon")
     h1.top__name Souta Kusunoki
     div.top__social
       a.top__social-link(href="http://github.com/Umisyo")
-        img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/github-icon_xq1ml1.webp" width="4em" height="4em" alt="Github")
+        img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/github-icon_xq1ml1.webp" width="40" height="40" alt="Github")
       a.top__social-link-instagram(href="http://instagram.com/ihcamonoihS")
-        img(src="https://res.cloudinary.com/umisyo/image/upload/v1568269864/Portfolio/instagram-icon_pj3wv5.webp" width="4em" height="4em" alt="Instagram")
+        img(src="https://res.cloudinary.com/umisyo/image/upload/v1568269864/Portfolio/instagram-icon_pj3wv5.webp" width="40" height="40" alt="Instagram")
       a.top__social-link(href="http://twitter.com/ihcamonoihS")
-        img(src="https://res.cloudinary.com/umisyo/image/upload/v1568269864/Portfolio/instagram-icon_pj3wv5.webp" width="4em" height="4em" alt="Twitter")
+        img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269865/Portfolio/twitter-icon_iofxq1.webp" width="40" height="40" alt="Twitter")
       a.top__social-link(href="http://facebook.com/ihcamonoihS")
-        img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/facebook-icon_dwhvaj.webp" width="4em" height="4em" alt="Facebook")
+        img(src="https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568269864/Portfolio/facebook-icon_dwhvaj.webp" width="40" height="40" alt="Facebook")
 </template>
