c93c4e17d87eba20ba79e5bb72674bbc4f567b1d
Umisyo <s_kusunoki@yumemi.co.jp>
2019-09-16 19:25:29 +0900

    fix: single colon

--- src/pages/Skill/index.vue
+++ src/pages/Skill/index.vue
@@ -1,37 +1,47 @@
-<template lang="pug">
+<template lang='pug'>
     div.skill
         h2 Skills
-        CardList(:content_list="content_list")
+        CardList(:content_list='content_list')
 
 </template>
 
 <script>
 export default {
   components: {
-    CardList: () => import("~/components/CardList")
+    CardList: () => import('~/components/CardList')
   },
   data() {
     return {
       content_list: [
         {
-          img: 'https://res.cloudinary.com/umisyo/image/upload/v1568269864/Portfolio/icon_zmntgo.webp',
-          title: "Python3",
-          text: "Django, Flask, and other..."
+          img: 'https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568624473/Portfolio/Python-icon_e4sffr.png',
+          title: 'Python3',
+          text: 'Django, Flask, and other...'
         },
         {
-          title: "JavaScript",
-          text: "Vue/Nuxt, React, Express"
+          img: 'https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568624693/Portfolio/JavaScript-icon_vknmjz.png',
+          title: 'JavaScript',
+          text: 'Vue/Nuxt, React, Express'
         },
         {
-          title: "PHP",
-          text: "Laravel"
+          img: 'https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568624799/Portfolio/php-icon_o6n7sz.png',
+          title: 'PHP',
+          text: 'Laravel'
         },
         {
-          title: "HTML/CSS",
-          text: "sass, bem"
+          img: 'https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568624904/Portfolio/html-icon_pmbe9n.png',
+          title: 'HTML/CSS',
+          text: 'sass, bem'
         },
         {
-          title: ""
+          img: 'https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568625092/Portfolio/Arduino-icon_ijzu3y.jpg',
+          title: 'Arduino',
+          text: 'development IoT'
+        },
+        {
+          img: 'https://res.cloudinary.com/umisyo/image/upload/f_auto/v1568625205/Portfolio/docker-icon_bmfmgq.png',
+          title: 'Docker',
+          text: 'Docker, docker-compose'
         }
       ]
     };
